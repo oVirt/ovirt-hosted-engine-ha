de054b885bfefb6af1f547354d07ba8f22d00801
Jenny Tokar <jtokar@redhat.com>
2016-12-07 12:39:29 +0200

    Refactor config.py
    Break the methods for loading and refreshing configuration
    files on to several smaller methods, that can be easily
    reused for other purposes.
    
    Change-Id: I0beff8c15fe6cd186bb87034c6ebe8a094a95eee
    Bug-Url: https://bugzilla.redhat.com/1301681
    Signed-off-by: Jenny Tokar <jtokar@redhat.com>

--- ovirt_hosted_engine_ha/env/config.py
+++ ovirt_hosted_engine_ha/env/config.py
@@ -100,35 +100,72 @@ class Config(object):
             HA: constants.HA_AGENT_CONF_FILE,
         }
 
-        self._load(Config.static_files)
+        self._shared_storage_files = {
+            VM: constants.HEConfFiles.HECONFD_VM_CONF
+        }
+
+        self._load_config_files(Config.static_files)
 
         if CONF_FILE in self._config[ENGINE]:
             self._dynamic_files.update({
                 VM: self._config[ENGINE][CONF_FILE]
             })
 
-    def _load(self, files):
-        for type, fname in files.iteritems():
-            conf = {}
+    def _load_single_conf_file(self, fname, config_type):
+        """
+        loads a single configuration file and writes its content
+        to the config dictionary.
+        :param fname: the file name to load
+        :param config_type: the type of the file name
+        """
+        conf = {}
 
-            try:
-                with open(fname, 'r') as f:
-                    for line in f:
-                        tokens = line.split('=', 1)
-                        if len(tokens) > 1:
-                            conf[tokens[0]] = tokens[1].strip()
-            except (OSError, IOError) as ex:
-                log = self._logger
+        try:
+            with open(fname, 'r') as f:
+                for line in f:
+                    tokens = line.split('=', 1)
+                    if len(tokens) > 1:
+                        conf[tokens[0].strip()] = tokens[1].strip()
+        except (OSError, IOError) as ex:
+            log = self._logger
 
-                if type in self._dynamic_files:
-                    level = log.debug
-                else:
-                    level = log.error
+            if config_type in self._dynamic_files \
+                    or config_type in self._shared_storage_files:
+                level = log.debug
+            else:
+                level = log.error
 
-                level("Configuration file '%s' not available [%s]", fname, ex)
+            level("Configuration file '%s' not available [%s]", fname, ex)
 
-            self._config.setdefault(type, {})
-            self._config[type].update(conf)
+        self._config.setdefault(config_type, {})
+        self._config[config_type].update(conf)
+
+    def _get_config_volume_path(self):
+        domain_type = self._config[ENGINE][DOMAIN_TYPE]
+        sdUUID = self._config[ENGINE][SD_UUID]
+        try:
+            conf_vol_uuid = self._config[ENGINE][CONF_VOLUME_UUID]
+            conf_img_uuid = self._config[ENGINE][CONF_IMAGE_UUID]
+        except (KeyError, ValueError):
+            if self._logger:
+                self._logger.debug("Configuration image doesn't exist")
+            return None
+
+        volumepath = heconflib.get_volume_path(
+            domain_type,
+            sdUUID,
+            conf_img_uuid,
+            conf_vol_uuid
+        )
+        return volumepath
+
+    def _load_config_files(self, conf_files):
+        """
+        loads the configuration files to the config dictionary.
+        :param conf_files: the files to load
+        """
+        for config_type, fname in conf_files.iteritems():
+            self._load_single_conf_file(fname, config_type)
 
     @property
     def _files(self):
@@ -139,7 +176,7 @@ class Config(object):
 
     def get(self, type, key, raise_on_none=False):
         if type in self._dynamic_files.keys():
-            self._load(dict([(type, self._dynamic_files[type])]))
+            self._load_config_files(dict([(type, self._dynamic_files[type])]))
 
         try:
             val = self._config[type][key]
@@ -173,9 +210,10 @@ class Config(object):
         with open(self._dynamic_files[type], 'r+') as f:
             fcntl.flock(f, fcntl.LOCK_EX)
 
-            # self._load() can re-open the exclusively-locked file because
-            # it's being called from the same process as the lock holder
-            self._load(dict([(type, self._dynamic_files[type])]))
+            # self._load_config_files() can re-open the exclusively-locked
+            # file because it's being called from the same process as the
+            # lock holder
+            self._load_config_files(dict([(type, self._dynamic_files[type])]))
             self._config[type][key] = str(value)
 
             text = ''
@@ -185,141 +223,126 @@ class Config(object):
             f.write(text)
             f.truncate()
 
-    def refresh_local_conf_file(self, localcopy_filename, archive_fname):
-        domain_type = self.get(ENGINE, DOMAIN_TYPE)
-        sd_uuid = self.get(ENGINE, SD_UUID)
-        conf_img_id = None
-        conf_vol_id = None
-        content = None
-        fallback = False
-        try:
-            conf_img_id = self.get(ENGINE, CONF_IMAGE_UUID)
-            conf_vol_id = self.get(ENGINE, CONF_VOLUME_UUID)
-        except (KeyError, ValueError):
-            if self._logger:
-                self._logger.debug("Configuration image doesn't exist")
-            pass
-
-        if not (conf_img_id and conf_vol_id):
-            if self._logger:
-                self._logger.debug(
-                    "Failing back to conf file from a previous release"
-                )
-            return False
-
-        if archive_fname == constants.HEConfFiles.HECONFD_VM_CONF:
-            if self._logger:
-                self._logger.info(
-                    "Trying to get a fresher copy of vm configuration "
-                    "from the OVF_STORE"
-                )
-
-            ovfs = ovf_store.OVFStore()
-            conf = None
-            scan = False
-
-            try:
-                scan = ovfs.scan()
-            except (EnvironmentError, Exception) as err:
-                self._logger.error(
-                    "Failed scanning for OVF_STORE due to %s",
-                    err
-                )
-
-            if scan:
-                heovf = ovfs.getEngineVMOVF()
-                if heovf is not None:
-                    self._logger.info(
-                        "Found an OVF for HE VM, "
-                        "trying to convert"
-                    )
-                    conf = ovf2VmParams.confFromOvf(heovf)
-                    if conf is not None:
-                        self._logger.info('Got vm.conf from OVF_STORE')
-                        content = conf
-                    else:
-                        self._logger.error(
-                            'Failed converting vm.conf from the VM OVF, '
-                            'falling back to initial vm.conf'
-                        )
-                else:
-                    self._logger.error(
-                        'Failed extracting VM OVF from the OVF_STORE '
-                        'volume, falling back to initial vm.conf'
-                    )
-            else:
-                self._logger.error(
-                    'Unable to identify the OVF_STORE volume, '
-                    'falling back to initial vm.conf. Please '
-                    'ensure you already added your first data '
-                    'domain for regular VMs'
-                )
-
-        if not content:
-            fallback = True
-            self._logger.error('Reading initial vm.conf')
-            source = heconflib.get_volume_path(
-                domain_type,
-                sd_uuid,
-                conf_img_id,
-                conf_vol_id,
-            )
-
-            if self._logger:
-                self._logger.debug(
-                    "Reading '{archive_fname}' from '{source}'".format(
-                        archive_fname=archive_fname,
-                        source=source,
-
-                    )
-                )
-            if heconflib.validateConfImage(self._logger, source):
-                content = heconflib.extractConfFile(
-                    self._logger,
-                    source,
-                    archive_fname,
-                )
-
+    def refresh_local_conf_file(self, config_type):
+        """
+        refreshes the local copy of the specified configuration file
+        :param config_type: the file to reload
+        :return: false if there was an error retrieving the file,
+                 true if the file was retrieved and updated.
+        """
+        content = self._get_file_content_from_shared_storage(
+            config_type
+        )
         if not content:
             if self._logger:
                 self._logger.debug(
-                    "unable to get a valid content"
+                    "unable to get a valid content, "
+                    "failing back to conf file from a previous release"
                 )
             return False
+
+        localcopy_filename = self._dynamic_files[config_type]
         if self._logger:
             self._logger.debug(
                 "Writing to '{target}'".format(
                     target=localcopy_filename,
                 )
             )
+
         with open(localcopy_filename, 'w') as target:
             target.write(content)
             if self._logger:
                 self._logger.debug(
                     "local conf file was correctly written"
                 )
-            if fallback:
-                return False
-            return True
+            target.truncate()
+
+        return True
+
+    def _get_vm_conf_content_from_ovf_store(self):
         if self._logger:
-            self._logger.error(
-                "Unable to get conf file"
+            self._logger.info(
+                "Trying to get a fresher copy of vm configuration "
+                "from the OVF_STORE"
             )
-            return False
+
+        ovfs = ovf_store.OVFStore()
+        scan = False
+
+        try:
+            scan = ovfs.scan()
+        except (EnvironmentError, Exception) as err:
+            self._logger.error(
+                "Failed scanning for OVF_STORE due to %s",
+                err
+            )
+
+        if scan:
+            heovf = ovfs.getEngineVMOVF()
+            if heovf is not None:
+                self._logger.info(
+                    "Found an OVF for HE VM, "
+                    "trying to convert"
+                )
+                conf = ovf2VmParams.confFromOvf(heovf)
+                if conf is not None:
+                    self._logger.info('Got vm.conf from OVF_STORE')
+                    return conf
+                else:
+                    self._logger.error(
+                        'Failed converting vm.conf from the VM OVF, '
+                        'falling back to initial vm.conf'
+                    )
+            else:
+                self._logger.error(
+                    'Failed extracting VM OVF from the OVF_STORE '
+                    'volume, falling back to initial vm.conf'
+                )
+        else:
+            self._logger.error(
+                'Unable to identify the OVF_STORE volume, '
+                'falling back to initial vm.conf. Please '
+                'ensure you already added your first data '
+                'domain for regular VMs'
+            )
+        return None
+
+    def _get_file_content_from_shared_storage(
+            self,
+            config_type
+    ):
+        config_volume_path = self._get_config_volume_path()
+        if not config_volume_path:
+            return None
+
+        archive_fname = self._shared_storage_files[config_type]
+        if self._logger:
+            self._logger.debug(
+                "Reading '{archive_fname}' from '{source}'".format(
+                    archive_fname=archive_fname,
+                    source=config_volume_path,
+                )
+            )
+        if heconflib.validateConfImage(self._logger, config_volume_path):
+            content = heconflib.extractConfFile(
+                self._logger,
+                config_volume_path,
+                archive_fname,
+            )
+        return content
 
     def refresh_vm_conf(self):
         if self._logger:
             self._logger.info(
                 "Reloading vm.conf from the shared storage domain"
             )
-        local_vm_conf_path = self.get(ENGINE, CONF_FILE)
-        if self.refresh_local_conf_file(
-            localcopy_filename=local_vm_conf_path,
-            archive_fname=constants.HEConfFiles.HECONFD_VM_CONF,
-        ):
-            mtime = monotonic.time()
-            self.vm_conf_refresh_time = int(mtime)
-            self.vm_conf_refresh_time_epoch = int(time.time() - mtime)
+
+        content = self._get_vm_conf_content_from_ovf_store()
+        if not content:
+            if self.refresh_local_conf_file(VM):
+                mtime = monotonic.time()
+                self.vm_conf_refresh_time = int(mtime)
+                self.vm_conf_refresh_time_epoch = int(time.time() - mtime)
 
     @classmethod
     def static_files_exist(cls):
