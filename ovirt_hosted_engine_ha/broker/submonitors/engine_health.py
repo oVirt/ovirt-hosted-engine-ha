46d736d414ccbb80eac26772621e7be3c074c1ce
Andrej Krejcir <akrejcir@redhat.com>
2017-09-14 13:35:59 +0200

    Fix wrong logger names in submonitors.
    Change-Id: I60ee1ce7003962815d65774d7207cc6128312c89
    Signed-off-by: Andrej Krejcir <akrejcir@redhat.com>

--- ovirt_hosted_engine_ha/broker/submonitors/engine_health.py
+++ ovirt_hosted_engine_ha/broker/submonitors/engine_health.py
@@ -40,7 +40,7 @@ def register():
 class Submonitor(submonitor_base.SubmonitorBase):
 
     def setup(self, options):
-        self._log = logging.getLogger("%s.CpuLoadNoEngine" % __name__)
+        self._log = logging.getLogger("%s.EngineHealth" % __name__)
         self._log.addFilter(log_filter.IntermittentFilter())
 
         self._vm_uuid = options.get('vm_uuid')
