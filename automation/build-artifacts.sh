df4be563373bb762df8d836915facfbc40f124a2
Sandro Bonazzola <sbonazzo@redhat.com>
2018-02-14 08:41:57 +0100

    build: post v2.1.9
    Change-Id: I23673b2f8e31d351d510375facbff0442222695c
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,12 +5,14 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 ./autogen.sh --system
 make dist
 yum-builddep ovirt-hosted-engine-ha.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-hosted-engine-ha-*.tar.gz
 
 mv *.tar.gz exported-artifacts
