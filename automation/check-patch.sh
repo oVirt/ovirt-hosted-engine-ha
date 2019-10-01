3568599c3184075e28dfec957c50a43c9e914163
Sandro Bonazzola <sbonazzo@redhat.com>
2019-10-01 12:25:52 +0200

    automation: avoid to build twice
    removing duplicate tar.gz avoiding to build
    the package twice on check-patch.
    
    Change-Id: I803aa723def3a8be8468d73d10a6c8c6a409c294
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -4,6 +4,7 @@ if [ `rpm -qv automake` != automake-1.16.1-5.fc29.noarch ]
 then
 # Workaround for http://bugzilla.redhat.com/1716384
 make distcheck
+rm -f *.tar.gz
 fi
 
 ./automation/build-artifacts.sh
