6c3eb6d59c750f9feea327edb6d2f587289863dd
Sandro Bonazzola <sbonazzo@redhat.com>
2019-10-01 14:08:16 +0200

    automation: add el8 builds
    Change-Id: Id218a226c3cc1a9c9627a13f04de7da611b868bd
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -1,8 +1,1 @@
-#!/bin/bash -e
-./autogen.sh --system
-./configure
-if [ `rpm -qv automake` != automake-1.16.1-5.fc29.noarch ]
-then
-# Workaround for http://bugzilla.redhat.com/1716384
-make distcheck
-fi
+check-patch.sh
