dafd78fed3a0abdaf53e072bf7f9e0fef1dc47d8
Sandro Bonazzola <sbonazzo@redhat.com>
2019-10-09 16:02:57 +0200

    automation: disable make distcheck
    automake 1.16.1 is afflicted by a regression causing
    make distcheck / make uninstall to fail.
    
    Change-Id: I658ac1675dedaeeabd7d4025a3e69f716615a508
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 ./autogen.sh --system
-if [ `rpm -qv automake` != automake-1.16.1-5.fc29.noarch ]
-then
-# Workaround for http://bugzilla.redhat.com/1716384
-make distcheck
-rm -f *.tar.gz
-fi
+
+# make distcheck skipped due to bug afflicting automake.
+# fc29: https://bugzilla.redhat.com/1716384
+# fc30: https://bugzilla.redhat.com/1757854
+# el8:  https://bugzilla.redhat.com/1759942
+# make distcheck
+# rm -f *.tar.gz
 
 ./automation/build-artifacts.sh
