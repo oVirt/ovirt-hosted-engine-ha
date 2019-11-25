56ec42d119981b954ed6270c1790f8d787a096a8
Evgeny Slutsky <eslutsky@redhat.com>
2019-11-25 13:56:22 +0200

    build: v2.4.0
    Change-Id: I1bb3efb089e7b43f757dd70aa847c768c8b4ad8f
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,8 +5,6 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
-
 if [ -x /usr/bin/python3 ] ; then
 export PYTHON=/usr/bin/python3
 fi
@@ -16,7 +14,6 @@ make dist
 yum-builddep ovirt-hosted-engine-ha.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-hosted-engine-ha-*.tar.gz
 
 mv *.tar.gz exported-artifacts
